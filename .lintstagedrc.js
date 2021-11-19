04688fdaa2dd60e3d1afd5cded4c628c221ba0d1
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-11-19 17:59:57 +0800

    new project

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,9 +3,4 @@ module.exports = {
     `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(' --file ')}`,
-  '**/*.less': (filenames) =>
-    ` stylelint --fix  ./${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(' --file ')}`,
 }
-// stylelint **/*.{css,less} --fix
