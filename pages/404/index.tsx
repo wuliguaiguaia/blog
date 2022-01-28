fb79ee3a86ee451f8bd9e0204d1e1843ddb97d93
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-01-28 13:43:43 +0800

    feat: 测试webhook

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,8 +1,8 @@
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
+// import Footer from '../../components/Footer'
+// import Header from '../../components/Header'
 import Head from '../../components/Head'
 import Link from 'next/link'
-import styles from './index.module.scss'
+// import styles from './index.module.scss'
 
 export default function Custom404() {
   return (
