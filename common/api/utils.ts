3ecb9f930841ee91f1f871827ca0539d150a3093
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-04-01 21:36:16 +0800

    fix: 发布

--- common/api/utils.ts
+++ common/api/utils.ts
@@ -5,7 +5,7 @@ import $http from '.'
  * 获取分类
  */
 export const getCategory = async () => {
-  const response = await $http.getcategorylist()
+  const response = await $http.getcategorylist({published: 1})
   const { data: { list } } = response
   return list
 }
@@ -23,7 +23,7 @@ export const getArticleList = async (
   other = {}
 ) => {
   params = { ...params, ...other }
-  const response = await $http.getarticlelist(params)
+  const response = await $http.getarticlelist({ ...params, published: 1 })
   const { data: { list, total } } = response
   return [list, total]
 }
@@ -33,7 +33,7 @@ export const getArticleList = async (
  */
 export const getArticleListFromSearch = async (params: { page: number; prepage: number; words: string | string[] | undefined }, other = {}) => {
   params = { ...params, ...other }
-  const response = await $http.search(params)
+  const response = await $http.search({ ...params, published: 1 })
   const { data: { list, total } } = response
   return [list, total]
 }
@@ -42,7 +42,7 @@ export const getArticleListFromSearch = async (params: { page: number; prepage: 
  * 获取文章详情
  */
 export const getArticle = async (params: { id: string | string[] | undefined }) => {
-  const { data } = await $http.getarticle(params)
+  const { data } = await $http.getarticle({ ...params, published: 1 })
   return data
 }
 
@@ -63,7 +63,7 @@ export const postComment = async (body: IComment) => {
 }
 
 /**
- * 发表评论
+ * 获取评论
  */
 export const getCommentList = async (articleId: number) => {
   const { data } = await $http.getcommentlist({ articleId })
--- common/plugins/axios.ts
+++ common/plugins/axios.ts
@@ -1,18 +1,20 @@
-import { loadingUtil } from './../../components/Loadingbar/index'
 import axios from 'axios'
-/* 
-  
-*/
+import { message } from 'antd'
 
 axios.interceptors.request.use((config) => {
   // loading start
   // loadingUtil.start()
   return config
 })
-axios.interceptors.response.use((response) => {
-  // loading end
-  // loadingUtil.end()
-  return response
+
+axios.interceptors.response.use((response) => response, (error) => {
+  const { data } = error.response
+  if (data?.errStr) {
+    message.error(data.errStr)
+  } else {
+    message.error(error.response.errorText)
+  }
+  return Promise.reject(error)
 })
 
 export default axios
