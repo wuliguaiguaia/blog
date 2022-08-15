85fc230aaa68e9dab29dc406adfd73b7668a5af7
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-08-15 22:12:11 +0800

    test

--- common/plugins/axios.ts
+++ common/plugins/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { message } from 'antd'
 
+
+axios.defaults.withCredentials = true
 axios.interceptors.request.use((config) => {
   // loading start
   // loadingUtil.start()
