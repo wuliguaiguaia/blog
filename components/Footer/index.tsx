6b2b56cbae2842def735d91609bce117c2ad26b9
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-09-16 14:56:16 +0800

    feat：添加备案号

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,9 @@ import styles from './index.module.scss'
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div><span>©️哇急急急哇制作 orangesolo.cn at 2022.2.21 </span></div>
+      <div>
+        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">陕ICP备2022000475号-1</a>
+      </div>
     </div>
   )
 }
