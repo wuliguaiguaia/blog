b7cd8ad71c1dfac185fe5d1842d062232f37f779
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-09-19 21:06:19 +0800

    feat: add copyright

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div>
+        <span>©哇唧唧哇制作啊哈哈哈快乐集团 </span>
         <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">陕ICP备2022000475号-1</a>
       </div>
     </div>
