fe2959e2f445c73db6da8e8c06e0aef290bf1b6e
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-11-18 16:47:42 +0800

    feat: next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const isDev = process.env.NODE_ENV === 'development'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
+const defaultConfig = {
   async rewrites() {
     return [
       {
@@ -17,4 +12,33 @@ module.exports = withPWA({
   plugins: [
     './common/plugins/axios.ts'
   ]
-})
+}
+
+module.exports = (function () {
+  if (isDev) {
+    return {
+      ...defaultConfig,
+      ... {
+        env: {
+          requestUrl: 'http://127.0.0.1:3009'
+        }
+      }
+    }
+  } else {
+    const withPWA = require('next-pwa')
+    const runtimeCaching = require('next-pwa/cache')
+
+    return withPWA({
+      ...defaultConfig,
+      ... {
+        env: {
+          requestUrl: 'https://orangesolo.cn'
+        },
+        pwa: {
+          dest: 'public',
+          runtimeCaching,
+        },
+      },
+    })
+  }
+})() 
